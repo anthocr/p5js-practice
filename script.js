c52834b2e3ab20cdb49096a74dbf5806d6184e27
Anthony Cruz <antho.cr04@gmail.com>
2025-06-20 18:00:26 +0000

    added p5js implementation

---
+++ script.js
@@ -0,0 +1,10 @@
+//setup runs initially and creates your canvas
+function setup() {
+    createCanvas(600, 600);
+    background(150);
+}
+
+// draw runs consitently and draws your elements
+function draw() {
+    circle(300, 300, 120)
+}
